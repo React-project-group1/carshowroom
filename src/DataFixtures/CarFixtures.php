e2b74dfc0d867e2869b80c76b3a2978613ca3c94
Danish <danishmujahid@outlook.com>
2023-03-27 10:34:12 +0100

    Seed data added

--- src/DataFixtures/CarFixtures.php
+++ src/DataFixtures/CarFixtures.php
@@ -40,5 +40,93 @@ class CarFixtures extends Fixture
         $car3->setImagePath('https://images.unsplash.com/photo-1624804269473-828dcc30a210?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1065&q=80');
         $manager->persist($car3);
         $manager->flush();
+
+        $car4 = new Car();
+        $car4->setModel('Phantom');
+        $car4->setMake('Rolls Royce');
+        $car4->setMileage(40000);
+        $car4->setCost(350000);
+        $car4->setEngineSize(6.6);
+        $car4->setDescription('This is a description of the Rolls Royce Phantom');
+        $car4->setImagePath('https://www.rolls-roycemotorcars.com/content/dam/rrmc/marketUK/rollsroycemotorcars_com/phantom-series-ii-in-detail/page-properties/01_RR_PHANTOM-single-card-min.jpg/jcr:content/renditions/cq5dam.web.1920.webp');
+        $manager->persist($car4);
+        $manager->flush();
+
+        $car5 = new Car();
+        $car5->setModel('Wraith');
+        $car5->setMake('Rolls Royce');
+        $car5->setMileage(10000);
+        $car5->setCost(280000);
+        $car5->setEngineSize(6.6);
+        $car5->setDescription('This is a description of the Rolls Royce Wraith');
+        $car5->setImagePath('https://media.autoexpress.co.uk/image/private/s--X-WVjvBW--/f_auto,t_content-image-full-desktop@1/v1608210716/autoexpress/2020/12/Rolls-Royce%20Wraith%20Kryptos%202020-3.jpg');
+        $manager->persist($car5);
+        $manager->flush();
+
+        $car6 = new Car();
+        $car6->setModel('M4 CSL');
+        $car6->setMake('BMW');
+        $car6->setMileage(2000);
+        $car6->setCost(140000);
+        $car6->setEngineSize(3.0);
+        $car6->setDescription('This is a description of the BMW M4 CSL');
+        $car6->setImagePath('https://cdn.bimmertoday.de/wp-content/uploads/2022/12/Manhart-BMW-M4-CSL-G82-Tuning-MH4-GTR-II-02.jpg');
+        $manager->persist($car6);
+        $manager->flush();
+
+        $car7 = new Car();
+        $car7->setModel('C63 S E');
+        $car7->setMake('Mercedes');
+        $car7->setMileage(2000);
+        $car7->setCost(120000);
+        $car7->setEngineSize(2.0);
+        $car7->setDescription('This is a description of the Mercedes C63 S E');
+        $car7->setImagePath('https://images.pistonheads.com/nimg/46601/blobid1.jpg');
+        $manager->persist($car7);
+        $manager->flush();
+
+        $car8 = new Car();
+        $car8->setModel('E63 S');
+        $car8->setMake('Mercedes');
+        $car8->setMileage(2000);
+        $car8->setCost(130000);
+        $car8->setEngineSize(4.0);
+        $car8->setDescription('This is a description of the Mercedes E63 S Final Edition');
+        $car8->setImagePath('https://cdn.hiconsumption.com/wp-content/uploads/2022/05/2023-Mercedes-AMG-E-63-S-4MATIC-Plus-Final-Edition-0-Hero.jpg');
+        $manager->persist($car8);
+        $manager->flush();
+
+        $car9 = new Car();
+        $car9->setModel('Huracan Evo');
+        $car9->setMake('Lamborghini');
+        $car9->setMileage(2000);
+        $car9->setCost(164000);
+        $car9->setEngineSize(5.2);
+        $car9->setDescription('This is a description of the Lamborghini Huracan Evo');
+        $car9->setImagePath('https://media.gq-magazine.co.uk/photos/5d13a6d57fcc8e3a89820ec9/16:9/w_2560%2Cc_limit/huracan-evo-04-gq-17apr18_b.jpg');
+        $manager->persist($car9);
+        $manager->flush();
+
+        $car10 = new Car();
+        $car10->setModel('Urus');
+        $car10->setMake('Lamborghini');
+        $car10->setMileage(500);
+        $car10->setCost(235000);
+        $car10->setEngineSize(4.0);
+        $car10->setDescription('This is a description of the Lamborghini Urus');
+        $car10->setImagePath('https://www.williamloughran.co.uk//media/7257/lamborghini-urus-80204-1.jpg');
+        $manager->persist($car10);
+        $manager->flush();
+
+        $car11 = new Car();
+        $car11->setModel('GT3 RS');
+        $car11->setMake('Porsche');
+        $car11->setMileage(200);
+        $car11->setCost(250000);
+        $car11->setEngineSize(3.8);
+        $car11->setDescription('This is a description of the Porsche GT3 RS');
+        $car11->setImagePath('https://f7432d8eadcf865aa9d9-9c672a3a4ecaaacdf2fee3b3e6fd2716.ssl.cf3.rackcdn.com/C2299/U1295/IMG_84137-large.jpg');
+        $manager->persist($car11);
+        $manager->flush();
     }
 }
