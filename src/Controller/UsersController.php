a69225e5bacd469ecf54c82551f121e19d0fc8c4
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-03-30 12:58:26 +0100

    Uncommented userscontroller for deploy

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -1,19 +1,19 @@
 <?php
-//
-//namespace App\Controller;
-//
-//use App\Entity\User;
-//use App\Repository\UserRepository;
-//use Doctrine\ORM\EntityManagerInterface;
-//use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-//use Symfony\Component\HttpFoundation\JsonResponse;
-//use Symfony\Component\HttpFoundation\Request;
-//use Symfony\Component\Routing\Annotation\Route;
-//
-//class UsersController extends AbstractController
-//{
-//    public function __construct(private UserRepository $userRepository) {}
-//
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UsersController extends AbstractController
+{
+    public function __construct(private UserRepository $userRepository) {}
+
 //    #[Route('/users', methods: ['POST'], name: 'post_user')]
 //    public function index(Request $request, EntityManagerInterface $entityManager): JsonResponse
 //    {
@@ -28,11 +28,11 @@
 //
 //        return $this->json( $user->toResponse() );
 //    }
-//
-//    #[Route('/users', methods: ['GET'], name: 'show_user')]
-//    public function show(Request $request): JsonResponse
-//    {
-//        $users = $this->userRepository->findAll();
-//        return $this->json( $users );
-//    }
-//}
+
+    #[Route('/users', methods: ['GET'], name: 'show_user')]
+    public function show(Request $request): JsonResponse
+    {
+        $users = $this->userRepository->findAll();
+        return $this->json( $users );
+    }
+}
