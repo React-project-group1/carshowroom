ce22d94f45b758902f2801e471c311df9f8baf74
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-04-11 13:43:23 +0100

    Removed more comments

--- src/Controller/CarsController.php
+++ src/Controller/CarsController.php
@@ -12,20 +12,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CarsController extends AbstractController
 {
-    public function __construct(private CarRepository $carRepository) {}// Entity manager usable for all functions (constructor)
+    public function __construct(private CarRepository $carRepository) {}
 
     // View all cars
     #[Route('/cars', methods: ['GET'], name: 'cars')]
     public function index(): JsonResponse
     {
-        // findAll() - SELECT * FROM cars
-        // find(param) - SELECT * FROM cars WHERE id = param
-        // findBy() - SELECT * FROM cars ORDER BY id DESC
-        // findOneBy() - SELECT * FROM cars WHERE id = 1 AND make = 'Ferrari' ORDER BY id DESC
-        // count() - return length/number of rows. SELECT COUNT() FROM cars WHERE id = 1
-        // getClassName() - returns current entity
-        // $repository = $this->em->getRepository(Car::class);
-
         $cars = $this->carRepository->findAll();
         return $this->json( $cars );
     }
